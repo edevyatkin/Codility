e725b5703adb1395a693dc04e28d8bde30678bcc
Evgeniy Devyatkin <mail@edevyatkin.com>
2018-07-14 22:34:03 +0300

    Lesson 2.2 solution

---
+++ Lesson 2.2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+// https://app.codility.com/demo/results/trainingKDMG7H-ZNP/
+
+namespace Lesson_2_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] A = { 9, 3, 9, 3, 9, 7, 9 };
+            var s = new Solution();
+            int result = s.solution(A);
+            Console.WriteLine(result);
+        }
+    }
+
+    class Solution {
+        public int solution(int[] A)
+        {
+            int result = 0;
+            foreach (int i in A)
+            {
+                result = result ^ i;
+            }
+            return result;
+        }
+    }
+}

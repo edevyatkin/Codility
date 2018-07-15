7171ef2d44f6d608b2a298bdedfc0b114ee59687
Evgeniy Devyatkin <mail@edevyatkin.com>
2018-07-15 12:37:58 +0300

    Lesson 3.1 solution

---
+++ FrogJmp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+// https://app.codility.com/demo/results/trainingWNATN7-Z8H/
+
+namespace FrogJmp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int X = 10;
+            int Y = 85;
+            int D = 30;
+
+            var s = new Solution();
+            int result = s.solution(X,Y,D);
+            Console.WriteLine(result);
+        }
+    }
+
+    class Solution
+    {
+        public int solution(int X, int Y, int D) =>
+            (int) Math.Ceiling((double)(Y - X) / D);
+    }
+}
